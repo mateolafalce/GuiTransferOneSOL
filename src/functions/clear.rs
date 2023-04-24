9f613a196bb34a18c788ad759b6c873144fcb0bc
Mateo Lafalce <mateolafalce3@gmail.com>
2023-04-25 00:47:59 +1100

    clear.rs

--- src/functions/clear.rs
+++ src/functions/clear.rs
@@ -9,11 +9,17 @@ use std::{
     },
     io::Error,
 };
-use crate::functions::Wallet;
+use crate::functions::Wallet; // Import Wallet struct from crate functions module
 
-pub fn clear() -> Result<(), Error> {
-    let mut wallet: Wallet = from_str(&read_to_string("src/wallet.json").unwrap()).unwrap();
-    wallet.wallet = vec![];
-    write("src/wallet.json", to_string(&wallet).unwrap()).unwrap();
+pub fn clear() -> Result<(), Error> { // Declare a function named clear that returns a Result type with empty tuple and Error type
+    let mut wallet: Wallet = from_str(
+        &read_to_string("src/wallet.json").unwrap()
+    ).unwrap(); // Read wallet.json file and deserialize its content to a Wallet struct variable named wallet
+    wallet.wallet = vec![]; // Assign an empty vector to wallet.wallet
+    write(
+        "src/wallet.json",
+        to_string(&wallet
+        ).unwrap())
+    .unwrap(); // Serialize wallet struct to a string and write it to wallet.json file
     Ok(())
 }
